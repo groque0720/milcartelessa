c57979f579823f814298d1cf76ebeaa2cd02316a
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2021-03-13 13:22:01 -0300

    Update app.blade.php

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -89,7 +89,7 @@
 
             let animado = document.querySelectorAll('.animado');
             function mostrarScroll(){
-                let scrollTop = document.documentElement.scrollTop+450;
+                let scrollTop = document.documentElement.scrollTop+550;
                 for (var i = 0; i < animado.length; i++) {
                     let alturaAnimado = animado[i].offsetTop;
                     if (alturaAnimado < scrollTop){
